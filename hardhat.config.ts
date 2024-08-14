1e3264871692511fc77df4af8d8e0ee7e8e160dc
stewart <16131485+StewartYe@users.noreply.github.com>
2024-08-14 17:55:22 +0800

    deploy rsk result

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -16,6 +16,10 @@ const config: HardhatUserConfig = {
     }
   },
   networks: {
+    root_stock: {
+	url: `https://public-node.rsk.co`,
+        accounts: [DEPLOY_PRI_KEY]
+    },
     bevm: {
       url: `https://rpc-mainnet-1.bevm.io`,
       accounts: [DEPLOY_PRI_KEY],
