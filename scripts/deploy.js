6d8e3d52af6939da410e230a3906c7c039a2011e
stewart <16131485+StewartYe@users.noreply.github.com>
2024-09-25 14:48:24 +0800

    fixx bug

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,8 +3,17 @@ const { ethers, upgrades } = require("hardhat");
 async function main() {
   const OmnityPort = await ethers.getContractFactory("OmnityPortContract");
   const omnityPort = await upgrades.deployProxy(OmnityPort, ["0xef1A4FdB2D67350Ec777e75F76be08E3542b993f"]);
+
   await omnityPort.waitForDeployment();
   console.log("OmnityPort deployed to:", await omnityPort.getAddress());
 }
+/*async function main() {
+  const OmnityPort = await ethers.getContractFactory("OmnityPortContract");
+  const omnityPort = await upgrades.deployProxy(OmnityPort, ["0xef1A4FdB2D67350Ec777e75F76be08E3542b993f"], {
+    useDeployedImplementation: true
+  });
 
+  await omnityPort.waitForDeployment();
+  console.log("OmnityPort deployed to:", await omnityPort.getAddress());
+}*/
 main();
